dc6b490e4aaf66817f23aebd05ba439dee3881cb
Tyler Kennedy <tk@tkte.ch>
2018-02-21 03:06:44 -0500

    Better memory management for the mz_zip_archive associated with a ZipFile.

--- fasterzip.pyx
+++ fasterzip.pyx
@@ -1,45 +1,66 @@
-cimport cfasterzip
 import zipfile
 import contextlib
+from cpython.mem cimport PyMem_Malloc, PyMem_Free
+from cfasterzip cimport (
+    mz_zip_archive,
+    mz_zip_zero_struct,
+    mz_zip_reader_init_file,
+    mz_zip_get_error_string,
+    mz_zip_get_last_error,
+    mz_zip_end,
+    mz_zip_reader_get_num_files,
+    mz_zip_archive_file_stat,
+    mz_zip_reader_file_stat,
+    mz_zip_reader_extract_file_to_heap,
+    mz_free
+)
 
 
 cdef class ZipFile:
-    cdef cfasterzip.mz_zip_archive _zip_archive;
+    cdef mz_zip_archive* _zip_archive;
 
-    def __init__(self, file: bytes):
-        cfasterzip.mz_zip_zero_struct(&self._zip_archive)
-
-        status = cfasterzip.mz_zip_reader_init_file(
-            &self._zip_archive,
+    def __init__(self, file):
+        status = mz_zip_reader_init_file(
+            self._zip_archive,
             file,
             0
         )
         if not status:
             raise zipfile.BadZipFile(
-                cfasterzip.mz_zip_get_error_string(
-                    cfasterzip.mz_zip_get_last_error(&self._zip_archive)
+                mz_zip_get_error_string(
+                    mz_zip_get_last_error(self._zip_archive)
                 )
             )
 
+    def __cinit__(self):
+        self._zip_archive = <mz_zip_archive*> PyMem_Malloc(
+            sizeof(mz_zip_archive)
+        )
+        if not self._zip_archive:
+            raise MemoryError()
+
+        mz_zip_zero_struct(self._zip_archive)
+
     def __dealloc__(self):
-        cfasterzip.mz_zip_end(&self._zip_archive)
+        mz_zip_end(self._zip_archive)
+        PyMem_Free(self._zip_archive)
 
     def _file_count(self):
-        return cfasterzip.mz_zip_reader_get_num_files(&self._zip_archive)
+        return mz_zip_reader_get_num_files(self._zip_archive)
 
     def infolist(self):
-        cdef cfasterzip.mz_zip_archive_file_stat stat
+        cdef mz_zip_archive_file_stat stat
 
         for i in range(self._file_count()):
-            status = cfasterzip.mz_zip_reader_file_stat(
-                &self._zip_archive,
+            status = mz_zip_reader_file_stat(
+                self._zip_archive,
                 i,
                 &stat
             )
             if not status:
                 raise zipfile.BadZipFile(
-                cfasterzip.mz_zip_get_error_string(
-                    cfasterzip.mz_zip_get_last_error(&self._zip_archive)
+                mz_zip_get_error_string(
+                    mz_zip_get_last_error(self._zip_archive)
                 )
             )
 
@@ -52,9 +73,9 @@ cdef class ZipFile:
         yield from (s['m_filename'] for s in self.infolist())
 
     @contextlib.contextmanager
-    def read(self, name):
+    def read(self, path):
         """
-        Reads the contents of `name`, returning a memory view over
+        Reads the contents of `path`, returning a memory view over
         it.
 
         .. note::
@@ -62,27 +83,27 @@ cdef class ZipFile:
             You should always use this as a context manager, as
             the buffer will be freed from the heap immediately after.
 
-        :param name:
-        :return: memory view of uncompressed contents.
+        :param path: The archive entry path.
+        :return: memory view of decompressed contents.
         """
         cdef void *p
         cdef size_t uncompressed_size
 
-        p = cfasterzip.mz_zip_reader_extract_file_to_heap(
-            &self._zip_archive,
-            name,
+        p = mz_zip_reader_extract_file_to_heap(
+            self._zip_archive,
+            path,
             &uncompressed_size,
             0
         )
         if not p:
             raise zipfile.BadZipFile(
-                cfasterzip.mz_zip_get_error_string(
-                    cfasterzip.mz_zip_get_last_error(&self._zip_archive)
+                mz_zip_get_error_string(
+                    mz_zip_get_last_error(self._zip_archive)
                 )
             )
 
         yield <char[:uncompressed_size]>p
-        cfasterzip.mz_free(p)
+        mz_free(p)
 
     def __len__(self):
         return self._file_count()
